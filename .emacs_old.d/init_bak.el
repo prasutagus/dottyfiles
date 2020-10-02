2dee74ab7b1eca509cf781351175afd48a83a3f0
david wheeler <david@davidwheeler.me.uk>
2020-10-02 14:12:20 +0100

    added the emacs_old folder

---
+++ .emacs_old.d/init_bak.el
@@ -0,0 +1,15 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; For use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(org-babel-load-file (expand-file-name ".emacs.d/config.org"))
